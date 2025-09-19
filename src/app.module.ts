5d485103152cb29bdc95d548c373a44e965173d9
cglove <pitsanubomb@gmail.com>
2025-09-19 20:02:18 +0700

    feat: Add Interceptor and Exception for format response

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TicketController } from './ticket/ticket.controller';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { AllExceptionsFilter } from './utils/filter/AllExceptionsFilter';
+import { TransfromInterceptor } from './utils/interceptors/TransfromInterceptor';
 
 @Module({
   imports: [],
   controllers: [TicketController],
-  providers: [],
+  providers: [
+    // Interceptor
+    {
+      provide:APP_INTERCEPTOR,
+      useClass: TransfromInterceptor,
+    },
+    // Filter
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter
+    }
+  ],
 })
 export class AppModule {}
