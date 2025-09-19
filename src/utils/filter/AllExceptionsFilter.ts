5c147bc85e285c0230bb6fe0d452bc4c4c43eb28
cglove <pitsanubomb@gmail.com>
2025-09-19 20:02:18 +0700

    feat: Add Interceptor and Exception for format response

---
+++ src/utils/filter/AllExceptionsFilter.ts
@@ -0,0 +1,24 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from "@nestjs/common";
+import { HttpAdapterHost } from "@nestjs/core";
+
+// Cath all error and response
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+    constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+    
+    catch(exception: HttpException, host: ArgumentsHost) {
+        const { httpAdapter } = this.httpAdapterHost;
+
+        const ctx = host.switchToHttp();
+
+        const httpStatus = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR
+
+        const responseBody = {
+            statusCode: httpStatus,
+            timestamp: new Date().toISOString(),
+            path: httpAdapter.getRequestUrl(ctx.getRequest()),
+        }
+
+        httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+    } 
+}
---
+++ src/utils/interceptors/TransfromInterceptor.ts
@@ -0,0 +1,28 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+interface Response<T> {
+  statusCode: number;
+  method: string;
+  path: string;
+  data: T;
+}
+
+@Injectable()
+export class TransfromInterceptor<T> implements NestInterceptor<T, Response<T>> {
+    intercept(
+        context: ExecutionContext,
+        next: CallHandler,
+    ):Observable<Response<T>> {
+        const req = context.switchToHttp().getRequest();
+        const res = context.switchToHttp().getResponse();
+        
+        return next.handle().pipe(map((data) => ({
+            statusCode: res.statusCode,
+            method: req.method,
+            path: req.path,
+            data
+        })));
+    }
+}
