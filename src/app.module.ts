8ff3e67d0d3548fd5330582b4f1e0cae987a9b97
cglove <pitsanubomb@gmail.com>
2025-09-21 18:53:05 +0700

    chore: Setting Que with bullmq in app modue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,33 @@
 import { Module } from '@nestjs/common';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import {ConfigModule} from '@nestjs/config'
+
 import { AllExceptionsFilter } from './utils/filter/AllExceptionsFilter';
 import { TransfromInterceptor } from './utils/interceptors/TransfromInterceptor';
 import { TicketModule } from './ticket/ticket.module';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
-  imports: [TicketModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // For call preoce.env to config global
+    }),
+    BullModule.forRoot({
+      connection: {
+        host: 'localhost',
+        port: 6379
+      },
+      defaultJobOptions: {
+        removeOnComplete: true,
+        removeOnFail: false,
+        attempts: 3,
+        backoff: {
+          type: 'exponential',
+          delay: 1000,
+        }
+      }
+    }),
+    TicketModule],
   providers: [
     // Interceptor
     {
