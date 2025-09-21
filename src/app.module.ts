7317bbed587425e3c11474757c5e9096e2cbb5a9
cglove <pitsanubomb@gmail.com>
2025-09-21 22:47:52 +0700

    chore: Add board to configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
-import {ConfigModule} from '@nestjs/config'
+import { ConfigModule } from '@nestjs/config';
 
 import { AllExceptionsFilter } from './utils/filter/AllExceptionsFilter';
 import { TransfromInterceptor } from './utils/interceptors/TransfromInterceptor';
@@ -11,18 +11,19 @@ import { TicketModule } from './ticket/ticket.module';
     ConfigModule.forRoot({
       isGlobal: true, // For call preoce.env to config global
     }),
-    TicketModule],
+    TicketModule,
+  ],
   providers: [
     // Interceptor
     {
-      provide:APP_INTERCEPTOR,
+      provide: APP_INTERCEPTOR,
       useClass: TransfromInterceptor,
     },
     // Filter
     {
       provide: APP_FILTER,
-      useClass: AllExceptionsFilter
-    }
+      useClass: AllExceptionsFilter,
+    },
   ],
 })
 export class AppModule {}
--- src/ticket/ticket.processor.ts
+++ src/ticket/ticket.processor.ts
@@ -1,42 +1,41 @@
-import { Processor, WorkerHost, OnWorkerEvent } from '@nestjs/bullmq'; 
-import { Injectable, Logger } from "@nestjs/common";
+import { Processor, WorkerHost } from '@nestjs/bullmq';
+import { Injectable, Logger } from '@nestjs/common';
 import { Job } from 'bullmq';
 
 @Processor('TicketNotifyJob')
 @Injectable()
 export class TicketProcessor extends WorkerHost {
-    private Logger = new Logger('Notification Processor Que')
-    async process(job: Job): Promise<void> {
-        const jobId = job.id;
-        const {ticketId, title, priority, status, type} = job.data;
+  private Logger = new Logger('Notification Processor Que');
+  async process(job: Job): Promise<void> {
+    const jobId = job.id;
+    const { ticketId, title, priority, status, type } = job.data;
 
-        this.Logger.log(`Process notification job ${jobId}`);
+    this.Logger.log(`Process notification job ${jobId}`);
 
-        // Mock Fail case logger fail
-        if(Math.random() <= 0.5) {
-            this.Logger.error(`Can't Notification job ${jobId} with process ${type}`);
-            throw new Error(`Mock fail send Notification case`);
-        }
-
-        const defaultLogMessage = `[JOBID: ${jobId}] Notification sent successfully ${type} ticket title: ${title} priority: ${priority} and status: ${status}`
-
-        switch (job.name) {
-            case 'CREATE':
-                this.Logger.log(defaultLogMessage);  
-                await job.updateProgress(100)
-
-            case 'READ':
-                this.Logger.log(`${defaultLogMessage} where ticketId: ${ticketId}`)
-                await job.updateProgress(100)
-            
-            case 'UPDATE':
-                this.Logger.log(`${defaultLogMessage} where ticketId: ${ticketId}`);
-                await job.updateProgress(100)
-
-            case 'DELETE':
-                this.Logger.log(`${defaultLogMessage} where ticketId: ${ticketId}`)
-                await job.updateProgress(100)
-        }
-
+    // Mock Fail case logger fail
+    if (Math.random() <= 0.5) {
+      this.Logger.error(`Can't Notification job ${jobId} with process ${type}`);
+      throw new Error(`Mock fail send Notification case`);
     }
+
+    const defaultLogMessage = `[JOBID: ${jobId}] Notification sent successfully ${type} ticket title: ${title} priority: ${priority} and status: ${status}`;
+
+    switch (job.name) {
+      case 'CREATE':
+        this.Logger.log(defaultLogMessage);
+        await job.updateProgress(100);
+
+      case 'READ':
+        this.Logger.log(`${defaultLogMessage} where ticketId: ${ticketId}`);
+        await job.updateProgress(100);
+
+      case 'UPDATE':
+        this.Logger.log(`${defaultLogMessage} where ticketId: ${ticketId}`);
+        await job.updateProgress(100);
+
+      case 'DELETE':
+        this.Logger.log(`${defaultLogMessage} where ticketId: ${ticketId}`);
+        await job.updateProgress(100);
+    }
+  }
 }
